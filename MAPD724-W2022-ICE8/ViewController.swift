f5ec840e007351e7e86abc00cffdc8b5694658b6
Ishtiaque Ahmed <iahmed85@my.centennialcollege.ca>
2022-03-25 16:00:06 -0400

    Initial Commit

---
+++ MAPD724-W2022-ICE8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD724-W2022-ICE8
+//
+//  Created by Ishtiaque Ahmed on 3/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
