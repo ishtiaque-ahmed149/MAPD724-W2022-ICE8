36748fb0d547006d87326aa7ffa8757c75ff4f95
Ishtiaque Ahmed <iahmed85@my.centennialcollege.ca>
2022-03-26 11:32:42 -0400

    final commit worked with car

--- MAPD724-W2022-ICE8/ViewController.swift
+++ MAPD724-W2022-ICE8/ViewController.swift
@@ -4,16 +4,44 @@
 //
 //  Created by Ishtiaque Ahmed on 3/25/22.
 //
-
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let imagePath = Bundle.main.path(forResource: "car", ofType: "jpg")
+        let imageURL = NSURL.fileURL(withPath: imagePath!)
+        let modelFile = MobileNetV2()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+
+        let handler = VNImageRequestHandler(url: imageURL)
+        let request = VNCoreMLRequest(model: model, completionHandler: findResults)
+        try! handler.perform([request])
+
+
     }
 
+    func findResults(request: VNRequest, error: Error?) {
+       guard let results = request.results as?
+       [VNClassificationObservation] else {
+       fatalError("Unable to get results")
+       }
+       var bestGuess = ""
+       var bestConfidence: VNConfidence = 0
+       for classification in results {
+          if (classification.confidence > bestConfidence) {
+             bestConfidence = classification.confidence
+             bestGuess = classification.identifier
+          }
+       }
+       labelDescription.text = "Image is: \(bestGuess) with confidence \(bestConfidence) out of 1"
+    }
 
 }
 
